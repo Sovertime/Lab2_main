0affc8b80ac04f163e29ae8f44d7aedc5cab45a1
Alexander <alexander@example.com>
2026-06-07 23:47:07 +0300

    lab2

---
+++ Laba2/Matrix.cs
@@ -0,0 +1,41 @@
+﻿namespace Laba2;
+
+public static class Matrix
+{
+    public static int[,] InputMatrix()
+    {
+        Console.Write("Введите количество строк: ");
+        int rows = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите количество столбцов: ");
+        int cols = int.Parse(Console.ReadLine());
+
+        int[,] matrix = new int[rows, cols];
+
+        Console.WriteLine("Введите элементы матрицы:");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < cols; j++)
+            {
+                Console.Write($"[{i},{j}] = ");
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+
+        return matrix;
+    }
+
+    public static void PrintMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Laba2/Program.cs
@@ -0,0 +1,16 @@
+﻿using Laba2;
+using Laba2.MatrixLibrary;
+
+int[,] matrix = Matrix.InputMatrix();
+
+Console.WriteLine("\nВведенная матрица:");
+Matrix.PrintMatrix(matrix);
+
+int mainSum = MatrixHelper.GetMainDiagonalSum(matrix);
+int secondarySum = MatrixHelper.GetSecondaryDiagonalSum(matrix);
+
+Console.WriteLine($"\nСумма главной диагонали: {mainSum}");
+Console.WriteLine($"Сумма побочной диагонали: {secondarySum}");
+
+Console.WriteLine("\nНажмите любую клавишу для выхода...");
+Console.ReadKey();
